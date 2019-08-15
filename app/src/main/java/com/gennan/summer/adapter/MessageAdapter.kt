25dbc85f36f7054d54f54bd0e13c161bec0aeb1f
Gennan <2018212778@stu.cqupt.edu.cn>
2019-08-15 17:45:35 +0800

    写了Toolbar 完善了一些细节 要想办法把AVUser或者是AVClient取出来 然后写聊天界面了

---
+++ app/src/main/java/com/gennan/summer/adapter/MessageAdapter.kt
@@ -0,0 +1,30 @@
+package com.gennan.summer.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ *Created by Gennan on 2019/8/15.
+ */
+class MessageAdapter : RecyclerView.Adapter<MessageAdapter.ViewHolder> {
+
+    constructor()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
